860942ce357461fe2c593e11f0d2c3c33f27fdbf
nknumaster <nknumaster@gmail.com>
2021-12-05 16:12:40 +0800

    ok

--- app.py
+++ app.py
@@ -36,12 +36,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if isinstance(event, MessageEvent):
-        message = quick_replyCon()
-        line_bot_api.reply_message(
-            event.reply_token,
-            message
-        )
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text)
+    )
 
 
 if __name__ == '__main__':
