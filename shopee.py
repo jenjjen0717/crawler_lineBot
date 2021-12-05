c325bf51b4d2f31a9c4eb493bc00b71a5fed940c
nknumaster <nknumaster@gmail.com>
2021-12-05 16:58:45 +0800

    ok

--- shopee.py
+++ shopee.py
@@ -9,6 +9,7 @@ from app import handle_postback
 DOMAIN = "https://shopee.tw/"
 keyword = input()
 price_max = input()
+conditions = handle_postback()
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36 Edg/96.0.1054.29',
