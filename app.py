78360321d4b8701fa21ff62727501f1689976fd3
nknumaster <nknumaster@gmail.com>
2021-12-06 23:25:07 +0800

    ok

--- app.py
+++ app.py
@@ -36,9 +36,9 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = event.source.user_id
-    line_bot_api.reply_message(
-        event.reply_token, msg)
+    # get user id when reply
+    user_id = event.source.user_id
+    print("user_id =", user_id)
 
 
 if __name__ == '__main__':
