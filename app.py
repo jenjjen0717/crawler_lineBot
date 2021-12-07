db3c3d77c5aaf22e26f51ac3eed2a57ccdcdc369
nknumaster <nknumaster@gmail.com>
2021-12-07 21:18:37 +0800

    ok

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = event.message.text.split(' ')
+    msg = event.message.text
     line_bot_api.reply_message(event.reply_token, msg)
 
 
