fa28e708ba98f3b50cec64e0868869fd25f99f75
nknumaster <nknumaster@gmail.com>
2021-12-07 22:32:30 +0800

    ok

--- app.py
+++ app.py
@@ -42,7 +42,7 @@ def callback():
 def handle_message(event):
     msg = event.message.text
     Message = shopee(msg)
-    line_bot_api.reply_message(event.reply_token, Message)
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(Text=msg))
 
 
 if __name__ == '__main__':
