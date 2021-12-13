9f3a9a76897ea49193a141eaf8c05137964f3b1a
nknumaster <nknumaster@gmail.com>
2021-12-14 02:14:31 +0800

    ok

--- app.py
+++ app.py
@@ -42,8 +42,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    message = shopeeTest()
-    line_bot_api.reply_message(event.reply_token, message)
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
 
 
 @handler.add(PostbackEvent)
