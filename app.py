8f25dfc52e3ed2b104d1860269642a9e846eff73
nknumaster <nknumaster@gmail.com>
2021-12-14 00:59:34 +0800

    ok

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ import os
 from config import *
 from shopee import *
 from shopee_item import shopee_option
+from test3 import *
 
 liff_api = LIFF(CHANNEL_ACCESS_TOKEN)
 
@@ -41,7 +42,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    message = shopee(msg)
+    message = shopeeTest()
     line_bot_api.reply_message(event.reply_token, message)
 
 
